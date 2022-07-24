3a505067a69462c8df6fb37126f2c399d5d81b4d
iamtushar <ecetushar22@gmail.com>
2022-07-24 12:44:49 +0530

    Template changes

--- IotApp/__init__.py
+++ IotApp/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from . import views
+from . import views, models
+
 
 
 
--- IotApp/models.py
+++ IotApp/models.py
@@ -1,39 +1,36 @@
-
-from sqlalchemy import *
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, sessionmaker
 
 engine = create_engine('sqlite:///openiot.db', echo=True)
 Base = declarative_base()
 
+# for creating a session
+
+Session = sessionmaker(bind=engine, autoflush=True)
+
 
 # Model Table Classes
 
+class JsonModel(object):
+    def _tojson(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
-class Device(Base):
+
+class Device(Base,JsonModel):
+    
     __tablename__ = 'device'
+    
     id = Column(Integer, primary_key = True)
-    ip = Column(String)
-    location = Column(String)
-    description = Column(String)
+    chip_id = Column(String(100), nullable = False)
+    location = Column(String(100))
+    description = Column(String(1000))
     is_active = Column(Integer)
     enrolled_at = Column(Date)
 
-    def __init__(self,id,ip,location,description,is_active,enrolled_at):
-        self.id = id
-        self.ip = ip
-        self.location = location
-        self.description = description
-        self.is_active = is_active
-        self.enrolled_at = enrolled_at
 
 
 
-
-
-
-if __name__=="__main__":
-    Base.metadata.create_all(engine)
+    
